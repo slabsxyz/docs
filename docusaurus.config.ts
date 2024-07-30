0076a6af8d68348a5c98dd78450646da78fc02fb
Felipe Flores <lffm.fismat@gmail.com>
2024-07-30 14:11:45 +0100

    fix broken link compilation problem and bad path issue

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,21 +4,15 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Security Labs',
-  tagline: 'Key Management Layer for Chain Abstraction',
-  favicon: 'img/logo.png',
-
-  // Set the production url of your site here
+  tagline: 'Key Management for Chain Abstraction',
+  favicon: 'img/favicon.ico',
   url: 'https://docs.securitylabs.xyz',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
+  baseUrl: '/docs/',
   organizationName: 'slabsxyz', // Usually your GitHub org/user name.
+  trailingSlash: true,
   projectName: 'docs', // Usually your repo name.
-
   onBrokenLinks: 'throw',
+  // onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -34,19 +28,19 @@ const config: Config = {
       'classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/slabsxyz/docs/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/slabsxyz/docs/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -54,80 +48,80 @@ const config: Config = {
     ],
   ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'Security Labs Docs',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        { to: '/blog', label: 'Blog', position: 'left' },
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Serendipity Global Inc. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+  // themeConfig: {
+  //   // Replace with your project's social card
+  //   image: 'img/docusaurus-social-card.jpg',
+  //   navbar: {
+  //     title: 'Security Labs Docs',
+  //     logo: {
+  //       alt: 'My Site Logo',
+  //       src: 'img/logo.svg',
+  //     },
+  //     items: [
+  //       {
+  //         type: 'docSidebar',
+  //         sidebarId: 'tutorialSidebar',
+  //         position: 'left',
+  //         label: 'Tutorial',
+  //       },
+  //       { to: '/blog', label: 'Blog', position: 'left' },
+  //       {
+  //         href: 'https://github.com/slabsxyz/docs',
+  //         label: 'GitHub',
+  //         position: 'right',
+  //       },
+  //     ],
+  //   },
+  //   footer: {
+  //     style: 'dark',
+  //     links: [
+  //       {
+  //         title: 'Docs',
+  //         items: [
+  //           {
+  //             label: 'Tutorial',
+  //             to: '/docs/intro',
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         title: 'Community',
+  //         items: [
+  //           // {
+  //           //   label: 'Stack Overflow',
+  //           //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+  //           // },
+  //           // {
+  //           //   label: 'Discord',
+  //           //   href: 'https://discordapp.com/invite/docusaurus',
+  //           // },
+  //           {
+  //             label: 'Twitter',
+  //             href: 'https://x.com/securitylabsxyz',
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         title: 'More',
+  //         items: [
+  //           {
+  //             label: 'Blog',
+  //             to: '/blog',
+  //           },
+  //           {
+  //             label: 'GitHub',
+  //             href: 'https://github.com/slabsxyz',
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //     copyright: `Copyright © ${new Date().getFullYear()} Serendipity Global Inc.`,
+  //   },
+  //   prism: {
+  //     theme: prismThemes.github,
+  //     darkTheme: prismThemes.dracula,
+  //   },
+  // } satisfies Preset.ThemeConfig,
 };
 
 export default config;
