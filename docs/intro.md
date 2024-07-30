89fb35a75bbe058586a4dd4a5a637c993fdf125a
Felipe Flores <lffm.fismat@gmail.com>
2024-07-30 23:04:44 +0100

    checkpoint with ata

--- docs/intro.md
+++ docs/intro.md
@@ -1,9 +1,9 @@
 ---
-# sidebar_position: 1
+sidebar_position: 1
 slug: /
 ---
 
-# Tutorial Intro!
+# Tutorial Intros!
 
-Let's discover **Security Labs in less than 5 minutes**.
+Let's discover **Security Labs in less than 5 minutes**. uwu
 
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Security Labs',
   tagline: 'Key Management for Chain Abstraction',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.svg',
   url: 'https://docs.securitylabs.xyz',
   baseUrl: '/docs/',
   organizationName: 'slabsxyz', // Usually your GitHub org/user name.
@@ -48,80 +48,87 @@ const config: Config = {
     ],
   ],
 
-  // themeConfig: {
-  //   // Replace with your project's social card
-  //   image: 'img/docusaurus-social-card.jpg',
-  //   navbar: {
-  //     title: 'Security Labs Docs',
-  //     logo: {
-  //       alt: 'My Site Logo',
-  //       src: 'img/logo.svg',
-  //     },
-  //     items: [
-  //       {
-  //         type: 'docSidebar',
-  //         sidebarId: 'tutorialSidebar',
-  //         position: 'left',
-  //         label: 'Tutorial',
-  //       },
-  //       { to: '/blog', label: 'Blog', position: 'left' },
-  //       {
-  //         href: 'https://github.com/slabsxyz/docs',
-  //         label: 'GitHub',
-  //         position: 'right',
-  //       },
-  //     ],
-  //   },
-  //   footer: {
-  //     style: 'dark',
-  //     links: [
-  //       {
-  //         title: 'Docs',
-  //         items: [
-  //           {
-  //             label: 'Tutorial',
-  //             to: '/docs/intro',
-  //           },
-  //         ],
-  //       },
-  //       {
-  //         title: 'Community',
-  //         items: [
-  //           // {
-  //           //   label: 'Stack Overflow',
-  //           //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-  //           // },
-  //           // {
-  //           //   label: 'Discord',
-  //           //   href: 'https://discordapp.com/invite/docusaurus',
-  //           // },
-  //           {
-  //             label: 'Twitter',
-  //             href: 'https://x.com/securitylabsxyz',
-  //           },
-  //         ],
-  //       },
-  //       {
-  //         title: 'More',
-  //         items: [
-  //           {
-  //             label: 'Blog',
-  //             to: '/blog',
-  //           },
-  //           {
-  //             label: 'GitHub',
-  //             href: 'https://github.com/slabsxyz',
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //     copyright: `Copyright © ${new Date().getFullYear()} Serendipity Global Inc.`,
-  //   },
-  //   prism: {
-  //     theme: prismThemes.github,
-  //     darkTheme: prismThemes.dracula,
-  //   },
-  // } satisfies Preset.ThemeConfig,
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Security Labs Docs',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/slabsxyz/docs',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          type: 'docsVersionDropdown',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            // {
+            //   label: 'Stack Overflow',
+            //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            // },
+            // {
+            //   label: 'Discord',
+            //   href: 'https://discordapp.com/invite/docusaurus',
+            // },
+            {
+              label: 'Twitter',
+              href: 'https://x.com/securitylabsxyz',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/slabsxyz',
+            },
+            {
+              label: 'Site',
+              href: 'https://www.securitylabs.xyz/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Serendipity Global Inc.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
 };
 
 export default config;
