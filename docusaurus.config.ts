3ede53b101a7fd8a24bd9cf9f45120427e300eb5
Felipe Flores <lffm.fismat@gmail.com>
2024-08-15 20:10:57 +0100

    allow fix of preview social card in telegram and changed to functor

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/functor_social_card.jpg',
     navbar: {
       title: 'Functor Network Docs',
       logo: {
